869ce72a89870ebf2637fe57c1d13c8cefe79e15
miqbalrr <iqb4lcakep@gmail.com>
2020-10-11 18:21:39 +0700

    init and define error handling

---
+++ src/meta_error.rs
@@ -0,0 +1,18 @@
+
+#[derive(Debug)]
+pub enum MetaError {
+    RequestError(reqwest::Error),
+    ParseError(serde_json::Error),
+}
+
+impl From<serde_json::Error> for MetaError {
+    fn from(e: serde_json::Error) -> Self {
+        Self::ParseError(e)
+    }
+}
+
+impl From<reqwest::Error> for MetaError {
+    fn from(e: reqwest::Error) -> Self {
+        Self::ParseError(e)
+    }
+}
