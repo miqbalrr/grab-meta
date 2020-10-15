ae5cd512ed3cac3c03a841b9cb09c662db4a7ef7
miqbalrr <iqb4lcakep@gmail.com>
2020-10-15 17:59:06 +0700

    add string error type

--- src/meta_error.rs
+++ src/meta_error.rs
@@ -3,6 +3,7 @@
 pub enum MetaError {
     RequestError(reqwest::Error),
     ParseError(serde_json::Error),
+    SomeError(String),
 }
 
 impl From<serde_json::Error> for MetaError {
@@ -14,5 +15,11 @@ impl From<serde_json::Error> for MetaError {
 impl From<reqwest::Error> for MetaError {
     fn from(e: reqwest::Error) -> Self {
         Self::RequestError(e)
+    }
+}
+
+impl From<String> for MetaError {
+    fn from(e: String) -> Self {
+        Self::SomeError(e)
     }
 }
