34ef3d2abfe6239bcc0bec1603cb56adf10a888a
miqbalrr <iqb4lcakep@gmail.com>
2020-10-15 23:27:07 +0700

    add comment

--- src/meta_error.rs
+++ src/meta_error.rs
@@ -1,4 +1,7 @@
 
+/*
+*  MetaError is an enum contains all error for this lib
+*/
 #[derive(Debug)]
 pub enum MetaError {
     RequestError(reqwest::Error),
@@ -6,18 +9,27 @@ pub enum MetaError {
     SomeError(&'static str),
 }
 
+/*
+* implement From for MetaError -> serde_json::Error
+*/
 impl From<serde_json::Error> for MetaError {
     fn from(e: serde_json::Error) -> Self {
         MetaError::ParseError(e)
     }
 }
 
+/*
+* implement From for MetaError -> reqwest::Error
+*/
 impl From<reqwest::Error> for MetaError {
     fn from(e: reqwest::Error) -> Self {
         MetaError::RequestError(e)
     }
 }
 
+/*
+* implement From for MetaError -> String / any error
+*/
 impl From<&'static str> for MetaError {
     fn from(e: &'static str) -> Self {
         MetaError::SomeError(e)
