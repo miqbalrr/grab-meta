f53f2356aadc056a47b0e0bdfe62227609e09593
miqbalrr <iqb4lcakep@gmail.com>
2020-10-22 01:26:50 +0700

    update release to crates.io

--- src/lib.rs
+++ src/lib.rs
@@ -5,19 +5,20 @@ mod meta;
 use select::document::Document;
 use site::{Site, Website, find_manual_meta, find_og_meta, find_tw_meta};
 
-pub fn get_meta(url: &'static str) -> String{ 
+pub fn get_meta(url: &'static str) -> Result<meta::Meta, meta_error::MetaError>{ 
     let mut site = Site::new(url);
-    site.check_type().unwrap();
+    site.check_type()?;
     let doc = Document::from(site.content.as_ref());
+    let default_meta = meta::Meta::new("kosong", "kosong", "kosong");
     let result: meta::Meta =  match site.meta_type {
-        meta::MetaType::Og => find_og_meta(&doc).unwrap(),
-        meta::MetaType::Tw => find_tw_meta(&doc).unwrap(),
-        meta::MetaType::Manual => find_manual_meta(&doc).unwrap(),
-        meta::MetaType::Facebook => find_manual_meta(&doc).unwrap(),
-        meta::MetaType::Twitter => find_manual_meta(&doc).unwrap(),
-        meta::MetaType::Instagram => find_og_meta(&doc).unwrap(),
+        meta::MetaType::Og => find_og_meta(&doc).unwrap_or(default_meta),
+        meta::MetaType::Tw => find_tw_meta(&doc).unwrap_or(default_meta),
+        meta::MetaType::Manual => find_manual_meta(&doc).unwrap_or(default_meta),
+        meta::MetaType::Facebook => find_manual_meta(&doc).unwrap_or(default_meta),
+        meta::MetaType::Twitter => return Err(meta_error::MetaError::SomeError("twitter site is not available at this momment")),
+        meta::MetaType::Instagram => find_og_meta(&doc).unwrap_or(default_meta),
     };
-    serde_json::to_string(&result).unwrap()
+    return Ok(result);
 }
 
 #[cfg(test)]
@@ -25,8 +26,7 @@ mod tests {
     use super::*;
     #[test]
     fn get_meta_test() {
-        let mymeta = get_meta("https://github.com/miqbalrr/grab-meta");
-        let mymeta: meta::Meta = serde_json::from_str(mymeta.as_str()).unwrap();
+        let mymeta = get_meta("https://github.com/miqbalrr/grab-meta").unwrap();
         assert_eq!(mymeta.title, "miqbalrr/grab-meta");
     }
 }
